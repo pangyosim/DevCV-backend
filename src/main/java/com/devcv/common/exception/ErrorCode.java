4975f6d0068dcd70addcffcb64f735806c22d885
luxihua <youngseoma@naver.com>
2024-06-05 16:27:31 +0900

    🔧[feat] common exception 추가, s3config 추가 #11

--- src/main/java/com/devcv/common/exception/ErrorCode.java
+++ src/main/java/com/devcv/common/exception/ErrorCode.java
@@ -5,6 +5,9 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
     //400
+    EMPTY_FILE_EXCEPTION("입력받은 이미지 파일이 빈 파일입니다."),
+    NO_FILE_EXTENTION("파일 확장자가 없습니다."),
+    INVALID_FILE_EXTENTION("유효하지 않은 파일 확장자입니다."),
 
     //401
 
@@ -13,6 +16,8 @@ public enum ErrorCode {
     //404
 
     //500
+    IO_EXCEPTION_ON_IMAGE_UPLOAD("이미지 업로드 중 IO 예외가 발생했습니다."),
+    PUT_OBJECT_EXCEPTION("S3에 객체를 저장하는 중 예외가 발생했습니다."),
     INTERNAL_SERVER_ERROR("서버 내부에 문제가 발생했습니다."),
     TEST_ERROR("테스트용 에러입니다."),
     ;
