9058b2d27e3efe7788414388b28f1b09357aec73
vks <spg9468@naver.com>
2024-06-08 09:53:32 +0900

    🦺 [feat] MemberResponse 기능수정(#12,#19)
    [feat] MemberResponse 기능수정(#12,#19)

--- src/main/java/com/devcv/member/domain/dto/MemberResponse.java
+++ src/main/java/com/devcv/member/domain/dto/MemberResponse.java
@@ -1,9 +1,16 @@
 package com.devcv.member.domain.dto;
 
 import com.devcv.member.domain.Member;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
+@AllArgsConstructor
 public class MemberResponse {
+
+    private Long userid;
     public static MemberResponse from(Member member) {
-        return null;
+        return new MemberResponse(member.getUserId());
     }
+
 }
