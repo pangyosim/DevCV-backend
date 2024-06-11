9e541ae40c3a29cbd4d5bb06911e34f4f31aa1a2
Taehyeonn <97taehyeon@gmail.com>
2024-06-12 00:08:28 +0900

    🐛 [Fix] deserialize 안되는 현상 수정, 오타 수정

--- src/main/java/com/devcv/member/domain/dto/MemberResponse.java
+++ src/main/java/com/devcv/member/domain/dto/MemberResponse.java
@@ -3,8 +3,10 @@ package com.devcv.member.domain.dto;
 import com.devcv.member.domain.Member;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class MemberResponse {
 
@@ -13,6 +15,6 @@ public class MemberResponse {
     private String userName;
     private String email;
     public static MemberResponse from(Member member) {
-        return new MemberResponse(member.getUserId(),member.getNickName(), member.getUserName(),member.getUserName());
+        return new MemberResponse(member.getUserId(),member.getNickName(), member.getUserName(), member.getEmail());
     }
 }
