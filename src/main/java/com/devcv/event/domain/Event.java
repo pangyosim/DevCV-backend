35e82e759be8b7279c066bf6fa95eac6ff62cd68
Taehyeonn <97taehyeon@gmail.com>
2024-06-14 16:39:20 +0900

    ✨ [Feat] event entity 설계 (#37)

---
+++ src/main/java/com/devcv/event/domain/Event.java
@@ -0,0 +1,40 @@
+package com.devcv.event.domain;
+
+import com.devcv.common.domain.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table(name = "tb_event")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Event extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private LocalDateTime startDate;
+
+    @Column
+    private LocalDateTime endDate;
+
+    private Event(String name, LocalDateTime startDate, LocalDateTime endDate) {
+        this.id = null;
+        this.name = name;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public static Event of(String name, LocalDateTime startDate, LocalDateTime endDate) {
+        return new Event(name, startDate, endDate);
+    }
+}
